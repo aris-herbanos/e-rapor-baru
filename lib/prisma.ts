f8aa184542eef0230728fc70a13e7f34510c570c
naufalhawari001-stack <avirtualzonecom@gmail.com>
2026-08-20 07:46:28 +0700

    lagi

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../prisma/generated/prisma';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 const connectionString = process.env.DATABASE_URL;
-
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
